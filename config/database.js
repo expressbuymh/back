8b4bad3a2612df11437c9b7949933a00e4f9b2d6
Rezk4 <alvarezmartinez132@gmail.com>
2023-06-04 14:49:58 -0500

    first_commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+//import mongoose from "mongoose";
+
+//mongoose.set('strictQuery', false)
+//mongoose.connect(process.env.MONGO)
+//    .then( () => console.log('database conected'))
+//    .catch(err => console.log(err) )
