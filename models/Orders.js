a93c19987123be64b7ca97fb2764cf549420ff4a
Mat <matiastejerina94@gmail.com>
2023-06-05 18:50:23 -0300

    Products e identacion

--- models/Orders.js
+++ models/Orders.js
@@ -1,21 +1,21 @@
 import mongoose from 'mongoose'
 
 const schema = new mongoose.Schema({
-  user_id: {
-    type: mongoose.Types.ObjectId,
-    ref: 'users',
-    required: true
-  },
-  /*  address_id: {
-    type: mongoose.Types.ObjectId,
-    ref: 'address',
-    required: true
-  }, */
-  status_order: { type: String, required: true },
-  products: [{ type: String, required: true }],
-  number_of_order: { type: String, required: true }
+    user_id: {
+        type: mongoose.Types.ObjectId,
+        ref: 'users',
+        required: true
+    },
+    /*  address_id: {
+      type: mongoose.Types.ObjectId,
+      ref: 'address',
+      required: true
+    }, */
+    status_order: { type: String, required: true },
+    products: [{ type: String, required: true }],
+    number_of_order: { type: String, required: true }
 }, {
-  timestamps: true
+    timestamps: true
 })
 const collection = 'orders'
 
