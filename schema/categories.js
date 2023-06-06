187e24eb32d77228d86ff84e8698311a4904920a
Cristian Sandoval <123500579+cris-sandoval@users.noreply.github.com>
2023-06-06 17:59:26 -0300

    Fixed (#22)
    * más productos
    
    * Crud department
    
    * normalizar

---
+++ schema/categories.js
@@ -0,0 +1,15 @@
+import Joi from "joi-oid"
+
+export const schema = Joi.object({
+    name: Joi
+        .string()
+        .required()
+        .min(3)
+        .max(20)
+        .message({
+            "string.min": "The name must have at least 3 characteres",
+            "string.max": "The name must have a maximum of 20 characteres",
+            'string.empty': 'The name cannot be empty',
+            'any.required': 'The name is required'
+        })
+})
