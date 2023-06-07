1c0c588d4313281231dcc5c80a381dc4491bcfcd
Facundo Cartolano <71531719+cartolanofacundo@users.noreply.github.com>
2023-06-07 11:39:20 -0300

    my orders (#25)
    * my orders
    
    * Update orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -1,11 +1,16 @@
 import { Router } from 'express'
+import my_orders from '../controllers/orders/my_orders.js'
 import getOrders from '../controllers/orders/getAll.js'
 import update from '../controllers/orders/updateStatus.js'
 import passport from '../middleware/passport.js'
 
+
+
 const router = Router()
 
+router.get("/myorders", passport.authenticate("jwt", {session: false}), my_orders)
 router.get('/', passport.authenticate('jwt', { session: false }), getOrders)
 router.put('/:id', passport.authenticate('jwt', { session: false }), update)
+
 
 export default router
