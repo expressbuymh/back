b02eceb1c66ae0ed4da308f4a8ef75f6578cf907
Cristian <alvarezmartinez132@gmail>
2023-06-07 10:41:52 -0500

    upd_prdts_control

--- controllers/products/products_filter.js
+++ controllers/products/products_filter.js
@@ -3,17 +3,32 @@ import Product from "../../models/Products.js";
 let filtered = async(req,res,next) => {
     try {
         let queries = {}
+        let pagination = {
+            limit: 5,
+            page: 1
+        }
+
         req.query.title ? queries.title = new RegExp(req.query.title.trim(), 'i') : ''
         req.query.category_id ? queries.category_id = req.query.category_id : ''
+        req.query.subCategory_id ? queries.subCategory_id = req.query.subCategory_id : '' 
+        //Armar logica para que el precio se maneje en rango(incluyendo)
+        req.query.price ? queries.price = req.query.price : ''
+
+        pagination.page = req.query.page ?? 1
+        pagination.limit = req.query.limit ?? 5
+
         console.log(queries)
-        let all = await Product.find(queries)
+        let all = await Product
+                    .find(queries)
+                    .skip(pagination.page > 0 ? (pagination.page-1)*pagination.limit : 0)
+                    .limit(pagination.limit > 0 ? pagination.limit : 0)
         return res.status(200).json({
             success: true,
             response: all
         })
     } catch (error) {
         console.log(error)
-        return req.status(500).json({
+        return res.status(500).json({
             error: 'Ha ocurrido un error'
         })
     }
