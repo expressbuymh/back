32c96ab5a65a744484c88f8096dfafef13586142
CATorres Varon <44732360+ctorresv@users.noreply.github.com>
2023-06-07 09:36:59 -0500

    Orders get all (#23)
    * carpetas de controllers y models (#2)
    
    * esquema de nuevos productos, Get y update de orders
    
    ---------
    
    Co-authored-by: Matias Tejerina <127890622+MatiasTejerina07@users.noreply.github.com>

--- routes/orders.js
+++ routes/orders.js
@@ -1,5 +1,11 @@
 import { Router } from 'express'
+import getOrders from '../controllers/orders/getAll.js'
+import update from '../controllers/orders/updateStatus.js'
+import passport from '../middleware/passport.js'
 
 const router = Router()
+
+router.get('/', passport.authenticate('jwt', { session: false }), getOrders)
+router.put('/:id', passport.authenticate('jwt', { session: false }), update)
 
 export default router
