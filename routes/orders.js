191769f13bf0e92eaacb547220f6b557bb6f76c7
cartolanofacundo <cartolanofacundo@gmail.com>
2023-06-07 11:35:26 -0300

    my orders

--- routes/orders.js
+++ routes/orders.js
@@ -1,5 +1,8 @@
 import { Router } from 'express'
-
+import passport from '../middleware/passport.js'
+import my_orders from '../controllers/orders/my_orders.js'
 const router = Router()
+
+router.get("/myorders", passport.authenticate("jwt", {session: false}), my_orders)
 
 export default router
