dd35afc32428c18418a89c49264693cfeb6f0b2e
Matias Tejerina <127890622+MatiasTejerina07@users.noreply.github.com>
2023-06-08 13:58:38 -0300

    checkout cart, endpoint (#43)

--- routes/carts.js
+++ routes/carts.js
@@ -4,6 +4,7 @@ import create from "../controllers/carts/create.js";
 import addProducts from "../controllers/carts/addProduct.js";
 import clearProduct from "../controllers/carts/clearProduct.js";
 import getmeCart from "../controllers/carts/getme_cart.js";
+import checkoutProduct from "../controllers/carts/checkout.js";
 const router = Router()
 
 
@@ -11,6 +12,7 @@ router.post('/', passport.authenticate('jwt', { session: false }), create)
 router.post('/addproducts/:id', passport.authenticate('jwt', { session: false }), addProducts)
 router.put('/clear/:id', passport.authenticate('jwt', { session: false }), clearProduct)
 router.get('/', passport.authenticate('jwt', { session: false }), getmeCart)
+router.put('/checkout/:id', checkoutProduct)
 
 
 export default router
