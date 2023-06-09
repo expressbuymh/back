85fd2ef92e86ed5ebea64d24a0408f0b24ca6c99
Matias Tejerina <127890622+MatiasTejerina07@users.noreply.github.com>
2023-06-09 18:48:14 -0300

    updatedepartments y endpoints (#58)

--- routes/departments.js
+++ routes/departments.js
@@ -2,14 +2,16 @@ import { Router } from "express";
 import create from "../controllers/departments/create.js";
 import read from "../controllers/departments/read.js";
 import update from "../controllers/departments/update.js";
-import destroy from "../controllers/departments/delete.js"
+import getAllDepartments from "../controllers/departments/getAllDepartments.js";
+import passport from '../middleware/passport.js'
 const router = Router()
 
 
 
-router.post('/', create)
+router.post('/', passport.authenticate('jwt', { session: false }), create)
+router.get('/admin', passport.authenticate('jwt', { session: false }), getAllDepartments)
 router.get('/', read)
-router.put('/:id', update)
-router.delete('/:id', destroy)
+router.put('/:id',passport.authenticate('jwt', { session: false }), update)
+
 
 export default router
