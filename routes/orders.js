bc38f25049f49f3a88e1d43d8a54e4b13a2260ee
Facundo Cartolano <71531719+cartolanofacundo@users.noreply.github.com>
2023-06-09 03:41:57 -0300

    orders/delivered (#50)

--- routes/orders.js
+++ routes/orders.js
@@ -4,6 +4,7 @@ import passport from '../middleware/passport.js'
 //controllers
 import statusPaid from '../controllers/orders/status_paid.js'
 import statusShipped from '../controllers/orders/status_shipped.js'
+import statusDelivered from '../controllers/orders/status_delivered.js'
 
 const router = Router()
 
@@ -11,6 +12,7 @@ const router = Router()
 //cambia la orden a paid
 router.put('/paid/:id', passport.authenticate('jwt',{session: false}), statusPaid)
 router.put('/shipped/:id', passport.authenticate('jwt',{session: false}), statusShipped)
+router.put('/delivered/:id', passport.authenticate('jwt',{session: false}), statusDelivered)
 
 
 
