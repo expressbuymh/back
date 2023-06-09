8cc4159596d2d0c4bda4754f1f69ee7270c89a7d
Cristian Álvarez <94159947+CristianAlvarezMartinez@users.noreply.github.com>
2023-06-09 16:48:37 -0500

    categoryServices and model with active property (#60)
    * carpetas de controllers y models (#2)
    
    * category_services
    
    * category_services
    
    ---------
    
    Co-authored-by: Matias Tejerina <127890622+MatiasTejerina07@users.noreply.github.com>
    Co-authored-by: Cristian <alvarezmartinez132@gmial.com>

---
+++ services/category.service.js
@@ -0,0 +1,88 @@
+import Category from "../models/Category.js";
+
+const categoryServices = {
+    create: async function(body) {
+        try {
+            let category = await Category.create(body)
+            return {
+                success: true,
+                status_code: 201,
+                category
+            }
+        } catch (error) {
+            return {
+                success: false,
+                status_code: 500,
+                message: [{
+                    path: 'create',
+                    message: 'There was an error while creating the category' 
+                }]
+            }
+        }
+    },
+    update: async function() {
+
+    },
+    read: async function(queries, pagination) {
+        try {
+            let all_categories = await Category
+                        .find(queries)
+                        .skip(pagination.page > 0 ? (pagination.page-1)*pagination.limit : 0)
+                        .limit(pagination.limit > 0 ? pagination.limit : 0)
+            return {
+                success: true,
+                status_code: 201,
+                all_categories
+            }
+        } catch (error) {
+            return {
+                success: false,
+                status_code: 500,
+                message: [{
+                    path: 'get_all',
+                    message: 'There was a error getting all categories'
+                }]
+            }
+        }
+    },
+    read_actives: async function() {
+        try {
+            let all_actives = await Category.find({active: true})
+            return {
+                success: true,
+                status_code: 201,
+                all_actives
+            }
+        } catch (error) {
+            return {
+                success: false,
+                status_code: 500,
+                message: [{
+                    path: 'read_actives',
+                    message: 'There was an error getting all actives categories'
+                }]
+            }
+        }
+    },
+    update: async function(id, body) {
+        try {
+            let updated_category = await Category.findByIdAndUpdate(id, body, { new: true })
+            return {
+                success: true,
+                status_code: 201,
+                updated_category
+            }
+        } catch (error) {
+            return {
+                success: false,
+                status_code: 500,
+                message: [{
+                    path: 'update',
+                    message: 'There was an error trying update the category'
+                }]
+            }
+        }
+    }
+}
+
+export default categoryServices
