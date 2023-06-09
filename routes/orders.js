d462759b85fa62e71fc8503237fa0442ec9cbc67
cartolanofacundo <cartolanofacundo@gmail.com>
2023-06-09 03:24:38 -0300

    shipped status

--- routes/orders.js
+++ routes/orders.js
@@ -3,12 +3,14 @@ import { Router } from 'express'
 import passport from '../middleware/passport.js'
 //controllers
 import statusPaid from '../controllers/orders/status_paid.js'
+import statusShipped from '../controllers/orders/status_shipped.js'
 
 const router = Router()
 
 
 //cambia la orden a paid
 router.put('/paid/:id', passport.authenticate('jwt',{session: false}), statusPaid)
+router.put('/shipped/:id', passport.authenticate('jwt',{session: false}), statusShipped)
 
 
 
--- services/product.service.js
+++ services/product.service.js
@@ -145,6 +145,7 @@ const productServices = {
             if (bd_product) {
                 if (bd_product.stock - product.quantity >= 0) {
                     bd_product.stock = bd_product.stock - product.quantity
+                    bd_product.sales = bd_product.sales + product.quantity
                     await bd_product.save()
                 } else {
                     let newError = {
