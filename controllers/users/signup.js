f46f8a9a741f3f543f27c913ef7aad8fcf8b9f89
Matias Tejerina <127890622+MatiasTejerina07@users.noreply.github.com>
2023-06-10 17:28:46 -0300

    arreglos en el signin, creacion del carrito de compras al crear el usuario, etc (#64)

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -1,10 +1,12 @@
 import usersServices from "../../services/user.service.js"
 import emailServices from "../../services/emails.service.js"
+import cartServices from '../../services/cart.service.js'
 
 const newUser = async (req, res, next) => {
   try {
     let response = await usersServices.sign_up(req.body)
     let sendEmail = emailServices.send_create_user(req.body, response.verify)
+    let createcart = cartServices.create(response.sign_up._id)
     return res.status(response.status_code).json({
       success: response.success,
       message: response.message,
