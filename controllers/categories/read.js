cd6634ca6632d59f54a63ca8c71252188a21ecc8
Cristian Álvarez <94159947+CristianAlvarezMartinez@users.noreply.github.com>
2023-06-12 09:21:39 -0500

    Queries_middles_activeProperty (#72)
    * carpetas de controllers y models (#2)
    
    * category_services
    
    * category_services
    
    * Queries_middles_active
    
    * Update verify_role_admin.js
    
    * Update verify_role_admin.js
    
    ---------
    
    Co-authored-by: Matias Tejerina <127890622+MatiasTejerina07@users.noreply.github.com>
    Co-authored-by: Cristian <alvarezmartinez132@gmial.com>

--- controllers/categories/read.js
+++ controllers/categories/read.js
@@ -7,15 +7,23 @@ const read = async(req,res,next) => {
         limit: 5,
         page: 1
     }
+
+    queries.name = new RegExp(req.query.name.trim(), 'i') ?? ''
+    if(req.query.department_id){
+      queries.department_id = req.query.department_id.split(',')
+    }
     pagination.page = req.query.page ?? 1
     pagination.limit = req.query.limit ?? 5
+
     let response = await categoryServices.read(queries, pagination)
     return res.status(response.status_code).json({
       success: response.success,
       message: response.message,
+      totalPages: Math.ceil(response.totalCount/pagination.limit),
       categories: response.all_categories
     })
   } catch (error) {
+    console.log(error)
     return res.status(500).json({
       success: false,
       message: [{
--- routes/categories.js
+++ routes/categories.js
@@ -7,15 +7,18 @@ import read from '../controllers/categories/read.js'
 import update from '../controllers/categories/update.js'
 import destroy from '../controllers/categories/delete.js'
 import read_actives from '../controllers/categories/read_actives.js'
+import verify_role_admin from '../middleware/categories/user_verification_middles/verify_role_admin.js'
+import isVerified from '../middleware/users/isVerified.js'
+import categoryExistsByName from '../middleware/categories/category_verification_middles/categoryExistsByName.js'  
 
 import {schema} from '../schema/categories.js'
 
 const router = Router()
 
-router.post('/',passport.authenticate('jwt', { session: false }),validator(schema), create)
-router.get('/admin', read)  //middle para verificar role
+router.post('/',passport.authenticate('jwt', { session: false }),validator(schema),verify_role_admin, isVerified, categoryExistsByName, create)
+router.get('/admin',passport.authenticate('jwt', { session: false }),verify_role_admin, isVerified ,read)  //middle para verificar role
 router.get('/', read_actives)
-router.put('/:id',passport.authenticate('jwt', { session: false }), update)
-router.delete('/:id',passport.authenticate('jwt', { session: false }), destroy)
+router.put('/:id',passport.authenticate('jwt', { session: false }), verify_role_admin, update)
+router.delete('/:id',passport.authenticate('jwt', { session: false }),verify_role_admin, destroy)
 
 export default router
