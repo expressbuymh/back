bfa80befc644f125feb81a5ed31b31abb2313b59
Cristian <alvarezmartinez132@gmail.com>
2023-06-12 09:16:45 -0500

    newProducts_categories_subCateg

--- models/data/subCategories.js
+++ models/data/subCategories.js
@@ -1,41 +1,51 @@
 export const subCategories =
 [{
+  active: true,
   category_id: 'Drinks',
   name: 'Soda'
 },
 {
+  active: true,
   category_id: 'Drinks',
   name: 'Juices'
 },
 {
+  active: true,
   category_id: 'Drinks',
   name: 'Ice tea'
 },
 {
+  active: false,
   category_id: 'Food pantry',
   name: 'Grain'
 },
 {
+  active: false,
   category_id: 'Food pantry',
   name: 'Oils and vinegars'
 },
 {
+  active: false,
   category_id: 'Vegetables and Fruits',
   name: 'Frozen vegetables'
 },
 {
+  active: false,
   category_id: 'Vegetables and Fruits',
   name: 'Vegetables'
 },
 {
+  active: false,
   category_id: 'Vegetables and Fruits',
   name: 'Fruits'
 },
 {
+  active: false,
   category_id: 'Meats and Fish',
   name: 'Meats'
 } ,
 {
+  active: true,
   category_id: 'Meats and Fish',
   name: 'Fish'
 }
