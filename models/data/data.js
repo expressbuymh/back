26701dbb0aaed920c3c28be1cd24b5023fab103b
Facundo Cartolano <71531719+cartolanofacundo@users.noreply.github.com>
2023-06-13 12:07:39 -0300

    data/products script para cargar productos y archivo de productos actualizado

--- models/data/data.js
+++ models/data/data.js
@@ -8,10 +8,13 @@ import { users } from './users.js'
 import Category from '../Category.js'
 import Department from '../Department.js'
 import Product from '../Product.js'
-import SubCategory from '../subCategory.js'
+import Subcategory from '../subCategory.js'
 import User from '../User.js'
+import Discount from '../Discount.js'
+import productSevices from '../../services/product.service.js'
 
-const newDepartment = async (departments) => await Department.insertMany(departments)
+
+/* const newDepartment = async (departments) => await Department.insertMany(departments)
 const newUser = async (users) => await User.insertMany(users)
 
 const newCategories = async (categories) => {
@@ -27,27 +30,38 @@ const newSubCategories = async (subCategories) => {
     sub.category_id = category._id
     await SubCategory.create(sub)
   }
-}
+} */
 
 const newProduct = async (products) => {
+  let i = 0
   for (const product of products) {
+    i++
+    console.log(i, product.name)
+    const discount_id = await Discount.create({ active: false, percentage: 1 })
     const department = await Department.findOne({ name: product.department_id })
     const category = await Category.findOne({ name: product.category_id })
-    const subCategory = await SubCategory.findOne({ name: product.subcategory_id })
+    const subCategory = await Subcategory.findOne({ name: product.subCategory_id})
+    product.discount_id = discount_id._id
     product.department_id = department._id
     product.category_id = category._id
-    console.log(subCategory)
     product.subcategory_id = subCategory._id
-    await Product.create(product)
+    await productSevices.create(product)
+    
+    /* await Product.create(product) */
   }
+
+
 }
 const data = async () => {
+  await newProduct(products);
+}
+/* const data = async () => {
   await newDepartment(departments)
   await newCategories(categories)
   await newUser(users)
   await newSubCategories(subCategories)
   await newProduct(products)
   console.log('Done!')
-}
+} */
 
 data()
