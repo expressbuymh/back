5eab729b9278bd333b80a4c72b19bc1affc3f49f
unknown <cris.sandoval.cf@gmail.com>
2023-06-14 01:46:04 -0300

    get-all

--- controllers/subcategories/getAllSubCategoryes.js
+++ controllers/subcategories/getAllSubCategoryes.js
@@ -2,13 +2,14 @@ import subcategoryServices from "../../services/subcategory.service.js";
 
 const getAllSubcategoryes = async (req, res, next) => {
     try {
-        let response = await subcategoryServices.get_all_subcategoryes()
+        let response = await subcategoryServices.get_all_subcategories()
         return res.status(response.status_code).json({
             success: response.success,
             message: response.message,
-            subcategoryes: response.subcategoryes
+            subcategories: response.subcategories
         })
     } catch (error) {
+        console.log(error);
         return res.status(500).json({
             success: false,
             message: [{
