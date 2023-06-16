4c595e71a8ba8450a0928026bccf66cb4625e88c
Facundo Cartolano <71531719+cartolanofacundo@users.noreply.github.com>
2023-06-15 22:18:50 -0300

    paid (#108)

--- controllers/paymments/sendPaymment.js
+++ controllers/paymments/sendPaymment.js
@@ -7,7 +7,7 @@ const sendPaymment = (req, res, next) => {
             items: [req.body],
             //el link es este "init_point": "https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=102965140-f8a87415-333a-4de0-bfeb-7b464d752da7"//
             back_urls: {
-                success: 'http://localhost:5173/payment/success',
+                success: 'https://front-web-six.vercel.app/payment/success',
                 failure: '',
                 pending: '',
             },
